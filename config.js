e0c6ab49f1688ff3edea38a5a59a70a92ee1ca83
somadatta9874 <somadatta9874@gmail.com>
2020-11-18 20:24:15 +0530

    changed config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
-import * as firebase from 'firebase'
-require('@firebase/firestore')
+import firebase from 'firebase/app'
+require('@firebase/app').default;
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBI_6KhkBRmacZ1wuIDJYA-H53CXyau-P0",
-    authDomain: "willy-app-f2f57.firebaseapp.com",
-    databaseURL: "https://willy-app-f2f57.firebaseio.com",
-    projectId: "willy-app-f2f57",
-    storageBucket: "willy-app-f2f57.appspot.com",
-    messagingSenderId: "957572252876",
-    appId: "1:957572252876:web:c77c38bb82a60591efee69",
-    measurementId: "G-4Y08PYMXK1"
+ 
+    apiKey: "AIzaSyD7SsQb0Mppki4OBZOpm_RNw0Sup5nK1hQ",
+    authDomain: "wily-48fb0.firebaseapp.com",
+    databaseURL: "https://wily-48fb0.firebaseio.com",
+    projectId: "wily-48fb0",
+    storageBucket: "wily-48fb0.appspot.com",
+    messagingSenderId: "72842777727",
+    appId: "1:72842777727:web:e55f4bb41db7bb0861db4a"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
